e214d9356acbd0d80e6027284ff27c2bc58e9521
dotansimha <dotansimha@gmail.com>
2017-06-24 10:00:05 +0300

    Step 3.3: Added basic template and logic to FollowListItem

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FollowListItemComponent } from './follow-list-item/follow-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FollowListItemComponent
   ],
   imports: [
     BrowserModule
--- client/src/app/follow-list-item/follow-list-item.component.ts
+++ client/src/app/follow-list-item/follow-list-item.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-follow-list-item',
   templateUrl: './follow-list-item.component.html',
   styleUrls: ['./follow-list-item.component.css']
 })
-export class FollowListItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class FollowListItemComponent {
+  @Input() user: any;
 }
