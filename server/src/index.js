69e1065bcc2063992a35d3e909815b6e7803a919
dotansimha <dotansimha@gmail.com>
2017-06-24 11:04:27 +0300

    Step 2.5: Added cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 
 const { Schema } = require('./schema');
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('tiny'));
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
