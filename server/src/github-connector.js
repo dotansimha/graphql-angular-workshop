45633d39eb3b337817a9a73e12c186f1fb0f478f
dotansimha <dotansimha@gmail.com>
2017-06-24 11:30:18 +0300

    Step 4.5: Adding a data loader to reduce number of requests to GitHub

--- server/src/github-connector.js
+++ server/src/github-connector.js
@@ -1,35 +1,47 @@
 const fetch = require('node-fetch');
+const DataLoader = require('dataloader');
 
 class GithubConnector {
-  constructor( accessToken ) {
+  constructor(accessToken) {
     this.accessToken = accessToken;
+    this.dataLoader = new DataLoader(this.fetchAll.bind(this), { batch: false });
   }
 
-  getUserForLogin( login ) {
+  getUserForLogin(login) {
     return this.getFromGithub(`/users/${login}`);
   }
 
-  getFollowingForLogin( login, page, perPage ) {
+  getFollowingForLogin(login, page, perPage) {
     return this.getFromGithub(`/users/${login}/following`, page, perPage);
   }
 
-  getFromGithub( relativeUrl, page, perPage ) {
+  getFromGithub(relativeUrl, page, perPage) {
     const url = `https://api.github.com${relativeUrl}?access_token=${this.accessToken}`;
-    return fetch(this.paginate(url, page, perPage)).then(res => res.json());
+
+    return this.dataLoader.load(this.paginate(url, page, perPage));
   }
 
-  paginate( url, page, perPage ) {
+  paginate(url, page, perPage) {
     let transformed = url.indexOf('?') !== -1 ? url : url + '?';
 
-    if ( page ) {
+    if (page) {
       transformed = `${transformed}&page=${page}`
     }
 
-    if ( perPage ) {
+    if (perPage) {
       transformed = `${transformed}&per_page=${perPage}`
     }
 
     return transformed;
+  }
+
+  fetchAll(urls) {
+    return Promise.all(
+      urls.map(url => {
+        console.log('Fetching Url', url);
+        return fetch(url).then(res => res.json())
+      })
+    );
   }
 }
 
