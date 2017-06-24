115138efc311304338cc7d3bb65e34f23caae760
dotansimha <dotansimha@gmail.com>
2017-06-24 10:55:10 +0300

    Step 3.5: Added FollowList with basic Observable implementation

--- client/src/app/follow-list/follow-list.component.ts
+++ client/src/app/follow-list/follow-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-follow-list',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./follow-list.component.css']
 })
 export class FollowListComponent implements OnInit {
+  private items$: Observable<any>;
 
   constructor() { }
 
   ngOnInit() {
+    this.items$ = Observable.of([
+      {
+        name: 'Dotan',
+        login: 'dotansimha',
+      },
+    ]);
   }
-
 }
